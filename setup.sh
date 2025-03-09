c9a24a50bd83ecf25c31768c8f7ce697733e3d73
Matt950918 <s111006@gm.fdhs.tyc.edu.tw>
2025-03-09 21:56:10 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # 更新系統包管理器並安裝必要工具
 apt-get update && apt-get install -y wget unzip
 
@@ -5,10 +7,15 @@ apt-get update && apt-get install -y wget unzip
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt install -y ./google-chrome-stable_current_amd64.deb
 
-# 獲取 Chrome 的主要版本並安裝對應版本的 ChromeDriver
+# 獲取 Chrome 的主版本
 CHROME_VERSION=$(google-chrome --version | awk '{print $3}' | cut -d '.' -f 1)
-wget https://chromedriver.storage.googleapis.com/${CHROME_VERSION}.0/chromedriver_linux64.zip
-unzip chromedriver_linux64.zip -d /usr/local/bin/
 
-# 清理下載的臨時文件
-rm google-chrome-stable_current_amd64.deb chromedriver_linux64.zip
+# 下載對應版本的 ChromeDriver
+wget https://chromedriver.storage.googleapis.com/${CHROME_VERSION}.0/chromedriver_linux64.zip
+if [ -f chromedriver_linux64.zip ]; then
+    unzip chromedriver_linux64.zip -d /usr/local/bin/
+    rm chromedriver_linux64.zip
+else
+    echo "ChromeDriver download failed"
+    exit 1
+fi
