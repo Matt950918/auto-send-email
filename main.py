08459556988030fb9349446f0b6651e5af68859e
Matt950918 <s111006@gm.fdhs.tyc.edu.tw>
2025-03-09 19:54:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -73,4 +73,4 @@ async def send_email(
     return {"status": "Email sending initiated", "stdout": result.stdout, "stderr": result.stderr}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=10000)
